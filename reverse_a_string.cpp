dc378c5a984607bc2d1188ffaf35b64777e90851
Harsh Raj J <harshraj201999@gmail.com>
2023-03-20 11:22:24 +0530

    Create reverse_a_string.cpp

---
+++ reverse_a_string.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+	
+	string str;
+	string str_rev;
+	cin>> str;
+	for(int i=str.size() -1; i>=0;str_rev.push_back(str[i--])){
+		
+	}
+	cout<<str_rev;
+
+}
