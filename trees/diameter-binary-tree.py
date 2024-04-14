2ded7ce8c9db1d4a8a48f1d462ec1d88adf98db0
Harsh Raj J <harshraj201999@gmail.com>
2024-04-14 14:48:16 -0700

    Update diameter-binary-tree.py

--- trees/diameter-binary-tree.py
+++ trees/diameter-binary-tree.py
@@ -1,3 +1,29 @@
+# current
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+        diameter = 0
+
+        def dfs(node):
+            nonlocal diameter
+            if node is None:
+                return 0
+
+            left = dfs(node.left)
+            right = dfs(node.right)
+            diameter = max(diameter, left + right)
+            return max(1 + left, 1 + right)
+
+        dfs(root)
+        return diameter
+
+
+# previous
 class Solution:
     def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
 
@@ -11,9 +37,9 @@ class Solution:
             else:
                 left = diabinary(root.left)
                 right = diabinary(root.right)
-                diameter[0] = max(diameter[0], left+right)
-        
-            return max(left+1, right+1)
+                diameter[0] = max(diameter[0], left + right)
+
+            return max(left + 1, right + 1)
 
         diabinary(root)
         return diameter[0]
